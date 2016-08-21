765ecb41d001815208c796ef29a34609801eab9a
Ahmed Rizwan <ahmed.rizwan@arkhitech.com>
2016-08-21 13:51:07 +0500

    Add sha

--- andgen.rb
+++ andgen.rb
@@ -3,7 +3,7 @@ class Andgen < Formula
   homepage "https://github.com/ahmedrizwan/andgen"
   url "https://github.com/ahmedrizwan/andgen/archive/v0.0.1.tar.gz"
   version "0.0.1"
-  sha256 ""
+  sha256 "119b47a73dc231d4685f2c5e70af322d7146a715fec0a1570caf03d3c281c449"
 
   def install
     bin.install "andgen"
