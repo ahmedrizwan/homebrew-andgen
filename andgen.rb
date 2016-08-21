7716397ec73b7c372ef67ef9a93a5056cab7ec59
Ahmed Rizwan <ahmed.rizwan@arkhitech.com>
2016-08-21 13:49:23 +0500

    Remove sha

--- andgen.rb
+++ andgen.rb
@@ -3,7 +3,7 @@ class Andgen < Formula
   homepage "https://github.com/ahmedrizwan/andgen"
   url "https://github.com/ahmedrizwan/andgen/archive/v0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "978c18f714ee8855baf1941860ff56f52c2c8d33ad3099e52dc04926c16e53ee"
+  sha256 ""
 
   def install
     bin.install "andgen"
