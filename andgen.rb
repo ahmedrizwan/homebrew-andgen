8d2cd9f9844a8c6a9b0b7e165a2cd100517bd7e7
Ahmed Rizwan <ahmed.rizwan@arkhitech.com>
2016-08-21 13:41:55 +0500

    Version 0.0.1

--- andgen.rb
+++ andgen.rb
@@ -1,8 +1,8 @@
 class Andgen < Formula
   desc "Templates generator for Android"
   homepage "https://github.com/ahmedrizwan/andgen"
-  url "https://github.com/ahmedrizwan/andgen/archive/v1.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/ahmedrizwan/andgen/archive/v0.0.1.tar.gz"
+  version "0.0.1"
   sha256 "978c18f714ee8855baf1941860ff56f52c2c8d33ad3099e52dc04926c16e53ee"
 
   def install
