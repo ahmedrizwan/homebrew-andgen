86a7a08a381a35c225eae9e159fda9dc0b1e5b88
Ahmed Rizwan <ahmed.rizwan@arkhitech.com>
2016-08-21 13:06:23 +0500

    Add desc and homepage

--- andgen.rb
+++ andgen.rb
@@ -1,6 +1,6 @@
 class Andgen < Formula
-  desc ""
-  homepage ""
+  desc "Templates generator for Android"
+  homepage "https://github.com/ahmedrizwan/andgen"
   url "https://github.com/ahmedrizwan/andgen/archive/v1.0.0.tar.gz"
   version "1.0.0"
   sha256 "00dcd0912d3ddd5352ed4f7705e320050f785ff98b5bbfc80aecc907e24a39ce"
